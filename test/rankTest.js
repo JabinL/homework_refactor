9cc18a5ee2edffb852dd406722c1a5594252af73
Jabin <2099008974@qq.com>
2020-08-29 00:03:30 +0800

    [homework-refactor][Jabin] add test rating case2 Voyage length is 5 and zone is west-indies. History length is 4

--- test/rankTest.js
+++ test/rankTest.js
@@ -253,3 +253,38 @@ rankTest(
       t.is("B", result);
     }
 );
+
+rankTest(
+    "rating case2 Voyage length is 5 and zone is west-indies. History length is 4",
+    (t) => {
+      //given
+      const voyage = {
+        zone: "east-indies",
+        length: 5,
+      };
+      const history = [
+        {
+          zone: "east-indies",
+          profit: 5,
+        },
+        {
+          zone: "west-indies",
+          profit: 15,
+        },
+        {
+          zone: "china",
+          profit: -2,
+        },
+        {
+          zone: "west-africa",
+          profit: 7,
+        },
+      ];
+
+      //when
+      const result = rating(voyage, history);
+
+      //then
+      t.is("B", result);
+    }
+);
