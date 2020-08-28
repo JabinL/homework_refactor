0ec1c79b1e380a22569983b007c81aa93f7b6cb2
Jabin <2099008974@qq.com>
2020-08-28 23:59:39 +0800

    [homework-refactor][Jabin] add test rating case 1 Voyage length is 10 and zone is west-indies. History length is 4

--- test/rankTest.js
+++ test/rankTest.js
@@ -218,3 +218,38 @@ rankTest('captainHistoryRisk case5: history.length > =5 vayage.zone === china hi
   const result = captainHistoryRisk(voyage,history);
   t.is(result,2 );
 });
+
+rankTest(
+    "rating case 1 Voyage length is 10 and zone is west-indies. History length is 4",
+    (t) => {
+      //given
+      const voyage = {
+        zone: "west-indies",
+        length: 10,
+      };
+      const history = [
+        {
+          zone: "east-indies",
+          profit: 5,
+        },
+        {
+          zone: "west-indies",
+          profit: 15,
+        },
+        {
+          zone: "china",
+          profit: -2,
+        },
+        {
+          zone: "west-africa",
+          profit: 7,
+        },
+      ];
+
+      //when
+      const result = rating(voyage, history);
+
+      //then
+      t.is("B", result);
+    }
+);
